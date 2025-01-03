36c93b75537cd73292bed32d6095ec7429523973
jackson <63016612+fairybow@users.noreply.github.com>
2025-01-03 12:45:26 -0500

    Update Main.cpp

--- AudioProjectTest/src/Main.cpp
+++ AudioProjectTest/src/Main.cpp
@@ -8,16 +8,19 @@
 #include "fftw3.h"
 
 // TODO:
-// Potentially build the library into the program instead
+// Potentially build the library into the program instead.
 // Determine library type and implement later. For now, just make console app.
 // It's also possible we only need console app.
-// Separate methods out into ns/classes, obvs
+// Separate methods out into ns/classes, obvs.
 
 // https://www.fftw.org/index.html
 
 // https://en.wikipedia.org/wiki/Voice_frequency
+// (Maybe:)
 // constexpr auto VOICE_HZ_MIN = 300;
 // constexpr auto VOICE_HZ_MAX = 3400;
+
+// Debug -----------------------------------------------------------------------
 
 #define THROW_RTE(message, ...)                                                \
     throwRte(std::source_location::current(), message, ##__VA_ARGS__)
@@ -44,8 +47,11 @@ static void throwRte(
 {
     auto formatted_message =
         std::vformat(messageFormat, std::make_format_args(args...));
+
     throwRte(file, line, function, formatted_message.c_str());
 }
+
+// ----------------------------------------------------------------------------
 
 static void read(const std::filesystem::path& audioFile)
 {
@@ -54,19 +60,19 @@ static void read(const std::filesystem::path& audioFile)
 
 // Read an audio file at path `in` and return an analysis as string (or throw an
 // exception)
-static std::string analyze(const std::filesystem::path& in)
+static std::string analyze(const std::filesystem::path& inFile)
 {
-    if (!std::filesystem::exists(in))
+    if (!std::filesystem::exists(inFile))
     {
-        THROW_RTE("\"{}\" does not exist.", in.string());
+        THROW_RTE("\"{}\" does not exist.", inFile.string());
     }
 
-    if (!std::filesystem::is_regular_file(in))
+    if (!std::filesystem::is_regular_file(inFile))
     {
-        THROW_RTE("\"{}\" is not regular file.", in.string());
+        THROW_RTE("\"{}\" is not regular file.", inFile.string());
     }
 
-    // Read audio file at path using libsndfile
+    // Read audio file at path
     // Get component frequencies using FFTW
     // Analyze
 
@@ -76,18 +82,8 @@ static std::string analyze(const std::filesystem::path& in)
 // Provide paths to sound files
 int main(int argc, char* argv[])
 {
-    try
-    {
-        std::cout << analyze("C:/Dev/Nonexistent.txt");
-    }
-    catch (const std::exception& ex)
-    {
-        std::cout << ex.what();
-        return 1;
-    }
-
     // argv[0] = executable path
-    /*for (auto i = 1; i < argc; ++i)
+    for (auto i = 1; i < argc; ++i)
     {
         try
         {
@@ -98,7 +94,7 @@ int main(int argc, char* argv[])
             std::cout << ex.what();
             return 1;
         }
-    }*/
+    }
 }
 
 #undef THROW_RTE
