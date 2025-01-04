ea4f5d8fa48bd56a2369833897dfa4ac83de6cf0
jackson <63016612+fairybow@users.noreply.github.com>
2025-01-03 23:26:56 -0500

    Update AudioAnalyzer.cpp

--- AudioProjectTest/src/AudioAnalyzer.cpp
+++ AudioProjectTest/src/AudioAnalyzer.cpp
@@ -5,7 +5,9 @@
 
 AudioAnalyzer::AudioAnalyzer()
 {
-    // Load or create the wisdom file
+    // Load the wisdom file (if present)
+    // Adjust it throughout the process, if needed, for different configurations.
+    // Right now, this may only be necessary for different FFT sizes?
 }
 
 std::vector<AudioAnalyzer::Analysis> AudioAnalyzer::process(const std::vector<std::filesystem::path>& paths, size_t fftSize)
